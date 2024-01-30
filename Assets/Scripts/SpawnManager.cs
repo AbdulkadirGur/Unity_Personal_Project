d8c72ea40c6bd35c956eb279b1ff7ac798991051
Abdulkadir Gür <58683068+AbdulkadirGur@users.noreply.github.com>
2024-01-30 16:55:04 +0300

    Creating basic gameplay for a project independently

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public Gameobject[] enemies;
-    public Gameobject[]  
+    public GameObject[] enemies;
+    public GameObject powerup;
+
+    private float zEnemySpawn = 10.0f;
+    private float xSpawnRange = 8.0f;
+    private float zPowerupRange = 2.0f;
+    private float ySwapnRange = 0.75f;
+
+    private float enemySpawnTime = 1.0f;
+    private float powerUpSpawnTime = 5.0f;
+    private float startDelay = 1.0f;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        InvokeRepeating("SpawnEnemy",startDelay ,enemySpawnTime);
+        InvokeRepeating("SpawnPowerUp",startDelay ,powerUpSpawnTime);
         
     }
 
@@ -17,5 +29,28 @@ public class SpawnManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void SpawnEnemy()
+    {
+        float randSpawnX = Random.Range(xSpawnRange, -xSpawnRange);
+        int randEnemy = Random.Range(0, enemies.Length);
+
+        Vector3 spawnPos = new Vector3(randSpawnX, ySwapnRange, zEnemySpawn);
+
+        Instantiate(enemies[randEnemy],spawnPos , enemies[randEnemy].transform.rotation);
+
+
+
+    }
+
+    void SpawnPowerUp()
+    {
+        float randSpawnX = Random.Range(xSpawnRange, -xSpawnRange);
+        float randSpawnZ = Random.Range(zPowerupRange, -zPowerupRange);
+
+        Vector3 spawnPos = new Vector3(randSpawnX, ySwapnRange, randSpawnZ);  
+
+        Instantiate(powerup,spawnPos , powerup.transform.rotation);
     }
 }
